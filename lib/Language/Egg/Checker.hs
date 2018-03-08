91a5bd4bc6fd6defdb7a8ee2764ecc68f42eacf5
Hanwen Zhang <i4640@live.com>
2018-03-07 21:44:18 -0800

    Update Checker.hs

--- lib/Language/Egg/Checker.hs
+++ lib/Language/Egg/Checker.hs
@@ -64,8 +64,9 @@ wellFormedE fEnv = go
     go vEnv (Let x e1 e2   _) = duplicateBindErrors vEnv x
                              ++ go vEnv e1
                              ++ go (addEnv x vEnv) e2
-    go vEnv (Tuple es      _) = error "TBD:wellFormed:Tuple"
-    go vEnv (GetItem e1 e2 _) = error "TBD:wellFormed:GetItem"
+    go vEnv (Tuple es      _) = gos vEnv es
+    go vEnv (GetItem e1 e2 _) = go  vEnv e1
+                             ++ go  vEnv e2
     go vEnv (App f es      l) = callArityErrors fEnv f es l
                              ++ unboundFunErrors fEnv f l
                              ++ gos vEnv es
